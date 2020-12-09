b7f1a7e0c3036f2d384e8c3758f9c11ebbcfd95e
ziSiNoapte <ksnapd@gmail.com>
2020-12-09 21:27:19 +0200

    Section 19 - pre switch to postgres

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,13 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        public BaseApiController()
-        {
-        }
+
     }
 }
